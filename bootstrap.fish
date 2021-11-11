31dfc3173d360f627661001a253ed37fc23382f0
Sriram Venkatesh <sriram.venkatesh@versent.com.au>
2021-11-11 14:21:43 +1100

    fix: 🐛 remove debug logging

--- bootstrap.fish
+++ bootstrap.fish
@@ -115,8 +115,7 @@ is_wsl
 and link_winhome
 
 set_universal_vars
-and success 'universal vars'
-or abort 'universal vars'
+
 
 curl -sL git.io/fisher | source && fisher install jorgebucaran/fisher
 and success 'fisher'
