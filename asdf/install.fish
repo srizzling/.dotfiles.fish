4daa27d34829f87fa5e1be73cb53ef8a80b21166
Sriram Venkatesh <sriram.venkatesh@shellenergy.com.au>
2023-08-17 10:51:02 +1000

    fix: 🐛 add tool versions symlink

--- asdf/install.fish
+++ asdf/install.fish
@@ -6,27 +6,33 @@ switch (uname)
         brew install asdf
     case '*'
         git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-        pushd ~/.asdf
-        set -l latesttag (git describe --abbrev=0 --tags)
-        git checkout $latesttag
-        popd
+        # pushd ~/.asdf
+        # set -l latesttag (git describe --abbrev=0 --tags)
+        # git checkout $latesttag
+        # popd
         set -Ux ASDF_HOME $HOME/.asdf
-end
 
-# unforently asdf doesn't support m1/m2 for awscli - so we are just skiping
-# for mac altogeather it is installed via brew instead
+        if not grep -q "source ~/.asdf/asdf.fish" ~/.config/fish/config.fish
+            echo "source ~/.asdf/asdf.fish" >>~/.config/fish/config.fish
+        end
+end
 
 
 # plugins to install
-set -l asdf_plugins nodejs golang python direnv saml2aws
+set -l asdf_plugins nodejs golang direnv saml2aws awscli exa starship jq gum rust
 for p in $asdf_plugins
     asdf plugin add $p
-end
-
-# set globals for all plugins to latest
-for p in $asdf_plugins
+    asdf install $p latest
     asdf global $p latest
 end
 
 # direnv requires special setup
 asdf direnv setup --shell fish --version latest
+
+# https://github.com/asdf-vm/asdf-nodejs#nvmrc-and-node-version-support
+ln -sf $DOTFILES/asdf/.asdfrc $HOME/.asdfrc
+ln -sf $DOTFILES/asdf/.default-npm-packages $HOME/.default-npm-packages
+ln -sf $DOTFILES/asdf/.tool-versions $HOME/.tool-versions
+
+# https://github.com/asdf-community/asdf-golang#goroot
+. ~/.asdf/plugins/golang/set-env.fish
