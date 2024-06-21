ff4684de0095215306a185ee9b091610c621f100
srizzling <sriram.venkatesh@versent.com.au>
2024-06-21 21:42:30 +1000

    fix: 🐛 fuck grc

--- lsd/install.fish
+++ lsd/install.fish
@@ -1,8 +1,7 @@
 #!/usr/bin/env fish
 
-if command -qa lsd
-    abbr -a ls 'lsd'
-    abbr -a l 'ls -l'
-    abbr -a la 'ls -a'
-    abbr -a lt 'ls --tree'
-end
+abbr -a ls 'lsd'
+abbr -a l 'ls -l'
+abbr -a la 'ls -a'
+abbr -a lt 'ls --tree'
+
