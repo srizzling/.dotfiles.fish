f983854643204dc6f1fa3410bbc1b3cb2aec20fb
Sriram Venkatesh <sriram.venkatesh@versent.com.au>
2025-01-06 14:52:13 +1100

    chore(iterm): 🧹 remove iterm config
    superseeded by ghostty
    
    Signed-off-by: Sriram Venkatesh <sriram.venkatesh@versent.com.au>

--- iterm/install.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-switch (uname)
-case Darwin
-	defaults write com.googlecode.iterm2 PrefsCustomFolder -string $DOTFILES_ROOT/iterm
-	defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
-end
