6c948c7cd7200b5d7f502b22d18adcdc80c655c7
Sriram Venkatesh <venksriram@gmail.com>
2025-08-08 10:43:46 +1000

    fix(delta): 🐛 resolve configuration conflict and integrate Catppuccin Macchiato theme
    Signed-off-by: Sriram Venkatesh <venksriram@gmail.com>

--- home-manager/delta.nix
+++ home-manager/delta.nix
@@ -1,20 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Configure delta with Catppuccin Macchiato theme
-  programs.git.delta = {
-    enable = true;
-    options = {
-      # Use Catppuccin Macchiato theme
-      features = "catppuccin-macchiato";
-      # Additional delta options for better display
-      navigate = true;
-      light = false;
-      side-by-side = true;
-      line-numbers = true;
-    };
-  };
-
   # Download and include Catppuccin themes for delta
   home.activation.installDeltaThemes = lib.hm.dag.entryAfter ["writeBoundary"] ''
     DELTA_THEMES_DIR="$HOME/.config/delta/themes"
@@ -27,11 +13,4 @@
         "https://github.com/catppuccin/delta/raw/main/catppuccin.gitconfig"
     fi
   '';
-
-  # Include the Catppuccin themes in git configuration
-  programs.git.includes = [
-    {
-      path = "~/.config/delta/themes/catppuccin.gitconfig";
-    }
-  ];
 }
--- home-manager/git.nix
+++ home-manager/git.nix
@@ -25,13 +25,17 @@
         navigate = true;
         light = false;
         line-numbers = true;
+        features = "catppuccin-macchiato";
       };
       
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
 
       # Include files for different contexts
-      include.path = "~/.gitconfig.local";
+      include.path = [
+        "~/.gitconfig.local"
+        "~/.config/delta/themes/catppuccin.gitconfig"
+      ];
       
       # Directory-based configuration
       "includeIf \"gitdir:~/development/personal/\"".path = "~/.gitconfig.personal";
